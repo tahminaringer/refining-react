b05a9317ac833c5d5be92da62675334506ef79f0
Tahmina <tahmina.ringer@gmail.com>
2021-05-03 16:54:54 -0700

    added api call and rendered to page

--- refining_react/src/App.js
+++ refining_react/src/App.js
@@ -1,10 +1,40 @@
+import React, {Component, useImperativeHandle} from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-     We will be back
+class App extends Component {
+
+  constructor(props) {
+    super(props)
+    // state
+    this.state = {
+      users: []
+    }
+  }
+
+  getUsers() {
+    axios('https://api.randomuser.me/?nat=US&results=5').then(response =>
+    //console log to check for api call
+    // .then(response => console.log(response));
+      this.setState({
+        users: response.data.results,
+      })
+    );
+  }
+
+  componentWillMount(){
+   this.getUsers();
+  }
+
+  render() {
+    return <div className="App">{this.state.users.map(user => 
+      <>
+        <h3>{user.name.first} {user.name.last}</h3>
+        <p>{user.cell}</p>
+        <p>{user.email}</p>
+      </>
+      )}
     </div>
-  );
+  };
 }
 
 export default App;
